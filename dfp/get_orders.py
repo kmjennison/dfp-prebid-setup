e52a569afdf266296aabaa3e9a4e819bd0c7d182
Kevin Jennison <kevin.jennison1@gmail.com>
2017-03-03 17:48:11 -0800

    Add function to get a DFP order by name.

--- dfp/get_orders.py
+++ dfp/get_orders.py
@@ -4,7 +4,46 @@ from googleads import dfp
 from dfp.client import get_client
 
 
-def main():
+def get_order_by_name(order_name):
+  """
+  Prints all orders in DFP.
+
+  Args:
+    order_name (str): the name of the DFP order
+  Returns:
+    a DFP order, or None
+  """
+
+  dfp_client = get_client()
+  order_service = dfp_client.GetService('OrderService', version='v201702')
+
+  # Filter by name.
+  query = 'WHERE name = :name'
+  values = [
+      {'key': 'name',
+       'value': {
+           'xsi_type': 'TextValue',
+           'value': order_name
+       }},
+  ]
+  statement = dfp.FilterStatement(query, values)
+  response = order_service.getOrdersByStatement(statement.ToStatement())
+
+  no_order_found = False
+  try:
+    no_order_found = True if len(response['results']) < 1 else False 
+  except (AttributeError, KeyError):
+    no_order_found = True
+
+  if no_order_found:
+    return None
+  else:
+    order = response['results'][0]
+    print('Order with ID "%d" and name "%s" was found.\n' % (order['id'],
+                                                             order['name']))
+    return order
+
+def get_all_orders():
   """
   Prints all orders in DFP.
 
@@ -34,6 +73,9 @@ def main():
       break
 
   print '\nNumber of results found: %s' % response['totalResultSetSize']
+
+def main():
+  get_all_orders()
 
 if __name__ == '__main__':
   main()
--- tests/test_dfp_get_orders.py
+++ tests/test_dfp_get_orders.py
@@ -19,7 +19,7 @@ class DFPServiceTests(TestCase):
       .GetService.return_value
       .getOrdersByStatement) = MagicMock()
 
-    dfp.get_orders.main()
+    dfp.get_orders.get_all_orders()
 
     # Confirm that it loaded the mock DFP client.
     mock_dfp_client.assert_called_once()
@@ -30,3 +30,97 @@ class DFPServiceTests(TestCase):
       .getOrdersByStatement.assert_called_once_with(expected_arg)
       )
 
+  def test_get_order_by_name(self, mock_dfp_client):
+    """
+    Ensure we make the correct call to DFP when getting an order
+    by name.
+    """
+    mock_dfp_client.return_value = MagicMock()
+    order_name = 'My Fake Order'
+
+    # Response for fetching orders.
+    (mock_dfp_client.return_value
+      .GetService.return_value
+      .getOrdersByStatement) = MagicMock()
+
+    dfp.get_orders.get_order_by_name(order_name)
+
+    # Expected argument to use in call to DFP.
+    expected_statement = {
+      'query': 'WHERE name = :name LIMIT 500 OFFSET 0',
+      'values': [{
+        'value': {
+          'value': order_name,
+          'xsi_type': 'TextValue'
+          },
+          'key': 'name'
+        }]
+    }
+
+    (mock_dfp_client.return_value
+      .GetService.return_value
+      .getOrdersByStatement.assert_called_once_with(expected_statement)
+      )
+
+  def test_get_order_by_name_return(self, mock_dfp_client):
+    """
+    Ensure we return the order when we get an order.
+    """
+    mock_dfp_client.return_value = MagicMock()
+    order_name = 'My Fake Order'
+
+    # Response for fetching orders.
+    (mock_dfp_client.return_value
+      .GetService.return_value
+      .getOrdersByStatement) = MagicMock(
+        return_value={
+          'totalResultSetSize': 1,
+          'startIndex': 0,
+          'results': [{
+            'id': 152637489,
+            'name': order_name,
+            'startDateTime': {},
+            'endDateTime': {},
+            'unlimitedEndDateTime': True,
+            'status': 'DRAFT',
+            'isArchived': False,
+            'externalOrderId': 0,
+            'currencyCode': 'USD',
+            'advertiserId': 123456789,
+            'creatorId': 123456789,
+            'traffickerId': 123456789,
+            'totalImpressionsDelivered': 0,
+            'totalClicksDelivered': 0,
+            'totalViewableImpressionsDelivered': 0,
+            'totalBudget': {
+              'currencyCode': 'USD',
+              'microAmount': 0,
+            },
+            'lastModifiedByApp': 'tab-for-',
+            'isProgrammatic': False,
+            'lastModifiedDateTime': {},
+          }]
+      }
+    )
+
+    order = dfp.get_orders.get_order_by_name(order_name)
+    self.assertEqual(order['id'], 152637489)
+
+  def test_get_no_order_by_name(self, mock_dfp_client):
+    """
+    Ensure we return None when an order does not exist.
+    """
+    mock_dfp_client.return_value = MagicMock()
+
+    # Response for fetching orders.
+    (mock_dfp_client.return_value
+      .GetService.return_value
+      .getOrdersByStatement) = MagicMock(
+        return_value={
+          'totalResultSetSize': 0,
+          'startIndex': 0,
+      }
+    )
+
+    order = dfp.get_orders.get_order_by_name('A new order')
+    self.assertIsNone(order)
