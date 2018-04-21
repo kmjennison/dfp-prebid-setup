e029d2d1e0ca3f9369524e30c95a80d6a65fdc66
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-21 10:37:01 -0700

    Add some checks of the order and line items

--- tests_integration/integration_test_new_prebid_partner.py
+++ tests_integration/integration_test_new_prebid_partner.py
@@ -58,13 +58,71 @@ class NewPrebidPartnerTests(TestCase):
   def test_new_partner(self):
 
     # TODO: add new bidder partner
+    print('Creating new bidder partner...')
+    print('New bidder partner created.')
+    print('Fetching created order and line items...')
 
     # Get the order and check that the order and line items
     # match what we'd expect.
+    # TODO: use generated order name
     order = get_order_by_name('Test April 5 2018')
     if order is None:
       self.fail('Order was not created.')
-    line_items = get_line_items_for_order(order['id'])
 
-    # TODO: check line items' structure
-    print(line_items[0:4])
+    # Validate the order
+    self.assertEqual(order['name'], 'Test April 5 2018')
+    self.assertEqual(order['status'], 'DRAFT')
+    # TODO: check advertiser ID
+
+    line_items = get_line_items_for_order(order['id'])
+    sorted_line_items = sorted(line_items, key=lambda li: li['costPerUnit']['microAmount'])
+
+    # Make sure line items match what we expect
+    print('Validating line items...')
+    # print(sorted_line_items[0:2])
+
+    num_line_items = 201
+    cpm_micro_amouts = map(lambda x: x * (10**5), range(num_line_items))
+    self.assertEqual(len(sorted_line_items), num_line_items)
+
+    for index, li in enumerate(sorted_line_items):
+      micro_amount = cpm_micro_amouts[index]
+
+      # Line item name
+      usd_val = float(micro_amount) / 10**6
+      usd_string = '%.{0}f'.format(str(2)) % usd_val
+      expected_name = 'testbidder: HB ${0}'.format(usd_string)
+      self.assertEqual(li['name'], expected_name)
+
+      # CPM
+      self.assertEqual(li['costPerUnit']['currencyCode'], 'USD')
+      self.assertEqual(li['costPerUnit']['microAmount'], micro_amount)
+
+      # Creative placeholders
+      creative_placeholders = sorted(li['creativePlaceholders'],
+        key=lambda cp: cp['size']['width'])
+      self.assertEqual(len(creative_placeholders), 2)
+      self.assertEqual(creative_placeholders[0]['size']['width'], 300)
+      self.assertEqual(creative_placeholders[0]['size']['height'], 250)
+      self.assertEqual(creative_placeholders[1]['size']['width'], 728)
+      self.assertEqual(creative_placeholders[1]['size']['height'], 90)
+
+      # Status
+      # TODO: eventually should have the tool activate line items
+      self.assertEqual(li['status'], 'DRAFT')
+      # TODO: reenable (some current test orders are archived)
+      # self.assertEqual(li['isArchived'], False)
+
+      # Targeting
+      targ = li['targeting']
+      self.assertEqual(targ['geoTargeting'], None)
+      self.assertEqual(targ['inventoryTargeting']['targetedAdUnits'], [])
+      self.assertEqual(targ['inventoryTargeting']['excludedAdUnits'], [])
+      # TODO: fetch and verify placement IDs
+      # self.assertEqual(targ['inventoryTargeting']['targetedPlacementIds'], [])
+
+      # TODO
+      # Check that custom targeting keys and values are correct
+
+    print('Line items validated.')
+
