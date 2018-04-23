987efa2503ee7534c5c6a6c4d1c5b32c75802b8b
Kevin Jennison <kevin.jennison1@gmail.com>
2018-04-23 13:22:58 -0700

    Debugging

--- dfp/client.py
+++ dfp/client.py
@@ -2,13 +2,9 @@
 from googleads import dfp
 
 import settings
-from settings import GOOGLEADS_YAML_FILE
 
 
 def get_client():
   print('Settings module file path:')
   print(settings.GOOGLEADS_YAML_FILE)
-
-  print('Imported file path:')
-  print(GOOGLEADS_YAML_FILE)
-  return dfp.DfpClient.LoadFromStorage(GOOGLEADS_YAML_FILE)
+  return dfp.DfpClient.LoadFromStorage(settings.GOOGLEADS_YAML_FILE)
