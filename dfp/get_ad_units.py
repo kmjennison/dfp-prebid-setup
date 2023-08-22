bb2967fc8653523a33031a75a1fc12c1ad1ca68d
mxra8 <lperezdiario@gmail.com>
2023-08-22 16:47:46 -0600

    Update libraries and dfp version

--- dfp/get_ad_units.py
+++ dfp/get_ad_units.py
@@ -27,7 +27,7 @@ def get_ad_unit_by_name(ad_unit_name):
 
   dfp_client = get_client()
   ad_unit_service = dfp_client.GetService('InventoryService',
-    version='v202208')
+    version='v202305')
 
   query = 'WHERE name = :name'
   values = [
